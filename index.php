5f788706574588e20442e4f5ac5f940f7e0b85ef
Henno Täht <henno.taht@gmail.com>
2014-03-15 22:41:47 +0200

    Add favicon

--- index.php
+++ index.php
@@ -32,6 +32,7 @@ if (date( "w", strtotime('now')) == 0) {
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" href="css/images/timetable.png" />
 		<title>Tunnid</title>
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<link href="css/jquery-ui-1.10.4.custom.min.css" rel="stylesheet">
