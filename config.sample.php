46736bce748c0e0177daf0fd714785cdfea17cad
Henno Täht <henno.taht@gmail.com>
2014-03-15 22:55:43 +0200

    Add config.sample.php

---
+++ config.sample.php
@@ -0,0 +1,11 @@
+<?php
+
+// Database config
+$user = "laura";
+$pass = "secret";
+$database = "tunnid";
+$server = "localhost";
+date_default_timezone_set('Europe/Tallinn');
+
+$loginFields = array('username' => 'svuser', 'password' => 'svpass', 'form'=> 'login', 'form_submit' => 'Kinnita');
+?>
